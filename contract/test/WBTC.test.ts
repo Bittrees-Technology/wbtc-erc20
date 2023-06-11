b141f27b2c941c0c30d67d6a937d3c6ab87c5e0a
Brian Gershon <briangershon@users.noreply.github.com>
2023-06-11 11:51:39 -0700

    Update tests to use WBTC contract

--- contract/test/WBTC.test.ts
+++ contract/test/WBTC.test.ts
@@ -3,13 +3,13 @@ import hre from 'hardhat';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { Contract } from 'ethers';
 
-describe('BTREEToken', function () {
+describe('WBTC', function () {
     let contract: Contract;
     let owner: SignerWithAddress;
     let otherUser: SignerWithAddress;
 
     beforeEach(async function () {
-        const Contract = await hre.ethers.getContractFactory('BTREEToken');
+        const Contract = await hre.ethers.getContractFactory('WBTC');
 
         const [_owner, _otherUser] = await hre.ethers.getSigners();
         owner = _owner;
